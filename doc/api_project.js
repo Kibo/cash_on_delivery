af5f1840149d7d08b973eec6d03d83a882834d7f
Tomas Jurman <tomasjurman@gmail.com>
2014-05-21 09:51:23 +0200

    0.9.2

--- doc/api_project.js
+++ doc/api_project.js
@@ -1,10 +1,10 @@
 define({
   "name": "PostForms",
-  "version": "0.4.0",
+  "version": "0.9.0",
   "description": "Web service for filling out the postal order form.",
   "apidoc": "",
   "generator": {
-    "version": "0.4.2",
-    "time": "2014-05-12T12:21:06.499Z"
+    "version": "0.4.3",
+    "time": "2014-05-21T07:50:07.053Z"
   }
 });
